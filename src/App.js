035e96b134692b3b11a721adcce663e65d3d1549
mariana <marianaxrp@gmail.com>
2021-03-20 00:05:21 -0300

    fixed layout problem on list component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
   render() {
     return (
