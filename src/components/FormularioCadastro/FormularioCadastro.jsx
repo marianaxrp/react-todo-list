f9433bd9ef18e52b0b24586f4de2866799250c82
mariana <marianaxrp@gmail.com>
2021-03-23 22:58:31 -0300

    refactored code

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,6 +7,17 @@ class FormularioCadastro extends Component {
     this.titulo = "";
     this.texto = "";
     this.categoria = "Sem categoria";
+    this.state = {
+      categorias: []
+    }
+  }
+
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({...this.state, categorias})
   }
 
   _handleMudancaCategoria(evento) {
@@ -38,8 +49,8 @@ class FormularioCadastro extends Component {
           className="form-cadastro_input"
         >
           <option>Sem categoria</option>
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>;
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>;
           })}
         </select>
         <input
